11bf26cd146f13227069721c676eda065278c823
NoahSewBSU <114342727+NoahSewBSU@users.noreply.github.com>
2025-04-09 16:10:07 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -9,3 +9,21 @@ QUnit.module('hello', function() {
 
 
 });
+
+// Create tests for...
+
+// Clicking on bottle runs code to switch to 'message-opened' view
+
+// Current-Message text box displays the current message in DB (use GET)
+
+// Max char is working
+
+// Bad language filter is working
+
+// Code injection filter is working
+
+// Button working
+
+// PUT command being registered by DB (use GET command, compare what was PUT)
+
+// 
